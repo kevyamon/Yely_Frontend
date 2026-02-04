d85c832349bf42145fdaeef15f9e6479bf34cdac
baticlean <baticlean225@gmail.com>
2026-02-04 01:13:08 +0000

    test pour utiliser sur tel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Autorise l'accès sur le réseau local
+    port: 5173,
+    strictPort: true, // Évite que Vite change de port si le 5173 est pris
+  }
 })
